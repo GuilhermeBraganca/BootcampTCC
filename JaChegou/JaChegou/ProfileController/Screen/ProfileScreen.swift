af2313556f1cfe80964aca9d45c919f4961b26f7
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-08-26 19:40:30 -0300

    Criando componentes da tela, Button, label, imageView

--- JaChegou/JaChegou/ProfileController/Screen/ProfileScreen.swift
+++ JaChegou/JaChegou/ProfileController/Screen/ProfileScreen.swift
@@ -11,6 +11,7 @@ protocol ProfileScreenProtocol: AnyObject {
     
     func tappedOutOfAccountButton()
     func tappedDeleteAccountButton()
+    func tappededitButton()
     
 }
 
@@ -33,6 +34,14 @@ class ProfileScreen: UIView {
         return label
     }()
     
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "E-mail"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        return label
+    }()
+    
     lazy var emailImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -42,6 +51,14 @@ class ProfileScreen: UIView {
         return imageView
     }()
     
+    lazy var phoneLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Telefone"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        return label
+    }()
+    
     lazy var phoneImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -49,6 +66,14 @@ class ProfileScreen: UIView {
         imageView.tintColor = .systemBlue
         imageView.contentMode = .scaleAspectFit
         return imageView
+    }()
+    
+    lazy var callendarLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Data de Nascimento"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
     }()
     
     lazy var callendarImageView: UIImageView = {
@@ -87,6 +112,28 @@ class ProfileScreen: UIView {
         return button
     }()
     
+//    lazy var editButton: UIButton = {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.setTitle("Editar", for: .normal)
+//        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+//        button.setTitleColor(.systemBlue, for: .normal)
+//        button.setTitle(UIImage(named: "pencil"), for: .normal)
+//        button.addTarget(self, action: #selector(tappededitButton), for: .touchUpInside)
+//        return button
+//    }()
+    
+    lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "pencil"), for: .normal)
+        button.setTitle("Editar", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(tappedEditButton), for: .touchUpInside)
+        return button
+    }()
+
     @objc func tappedOutOfAccountButton() {
         delegate?.tappedOutOfAccountButton()
         
@@ -95,6 +142,10 @@ class ProfileScreen: UIView {
     @objc func tappedDeleteAccountButton() {
         delegate?.tappedDeleteAccountButton()
     }
+    @objc func tappedEditButton() {
+        delegate?.tappedDeleteAccountButton()
+    }
+    
     
     init() {
         super.init(frame: .zero)
@@ -110,9 +161,13 @@ class ProfileScreen: UIView {
     func addElements() {
         addSubview(headerView)
         addSubview(personalInformationLabel)
+        addSubview(emailLabel)
         addSubview(emailImageView)
+        addSubview(phoneLabel)
         addSubview(phoneImageView)
+        addSubview(callendarLabel)
         addSubview(callendarImageView)
+        addSubview(editButton)
         addSubview(outOfAccountButton)
         addSubview(deleteAccountButton)
         
@@ -125,26 +180,38 @@ class ProfileScreen: UIView {
             headerView.trailingAnchor.constraint(equalTo: trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 200),
             
-            personalInformationLabel.topAnchor.constraint(equalTo: topAnchor, constant: 230),
+            personalInformationLabel.topAnchor.constraint(equalTo: topAnchor, constant: 240),
             personalInformationLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             personalInformationLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            emailLabel.topAnchor.constraint(equalTo: personalInformationLabel.bottomAnchor, constant: 50),
+            emailLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
             
             emailImageView.topAnchor.constraint(equalTo: personalInformationLabel.bottomAnchor, constant: 50),
             emailImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             emailImageView.heightAnchor.constraint(equalToConstant: 20),
             emailImageView.widthAnchor.constraint(equalToConstant: 20),
             
+            phoneLabel.topAnchor.constraint(equalTo: emailImageView.bottomAnchor, constant: 30),
+            phoneLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            
             phoneImageView.topAnchor.constraint(equalTo: emailImageView.bottomAnchor, constant: 30),
             phoneImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             phoneImageView.heightAnchor.constraint(equalToConstant: 20),
             phoneImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            callendarLabel.topAnchor.constraint(equalTo: phoneLabel.bottomAnchor, constant: 30),
+            callendarLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
             
             callendarImageView.topAnchor.constraint(equalTo: phoneImageView.bottomAnchor, constant: 30),
             callendarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             callendarImageView.heightAnchor.constraint(equalToConstant: 20),
             callendarImageView.widthAnchor.constraint(equalToConstant: 20),
             
-            outOfAccountButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -70),
+            editButton.topAnchor.constraint(equalTo: callendarImageView.bottomAnchor, constant: 30),
+            editButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            
+            outOfAccountButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -80),
             outOfAccountButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             outOfAccountButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             outOfAccountButton.heightAnchor.constraint(equalToConstant: 40),
